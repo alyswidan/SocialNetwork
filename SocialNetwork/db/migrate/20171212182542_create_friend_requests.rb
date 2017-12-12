295af938ea466bd9c01829acfc74a2fa69804d02
Admin <amrmohamed53@gmail.com>
2017-12-12 20:57:52 +0200

    friend requests migration

---
+++ SocialNetwork/db/migrate/20171212182542_create_friend_requests.rb
@@ -0,0 +1,15 @@
+class CreateFriendRequests < ActiveRecord::Migration[5.1]
+  def change
+    create_table :friend_requests do |t|
+      t.integer :user_id
+      t.integer :other_user_id
+      t.integer :state
+      t.timestamps
+    end
+    add_index :friend_requests, [:user_id,:other_user_id ], unique: true
+    add_foreign_key :friend_requests, :users, column: :user_id, primary_key: :id,on_delete: :cascade
+    add_foreign_key :friend_requests, :users, column: :other_user_id, primary_key: :id,on_delete: :cascade
+
+  end
+end
+
