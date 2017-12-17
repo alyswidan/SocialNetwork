72235db8346b32916ea050f95b9ccea018496f2e
alyswidan <swidan12340@gmail.com>
2017-12-17 19:35:49 +0200

    editing templete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
-//= require jquery
-//= require jquery-ui
 //= require rails-ujs
-
-//= require_tree
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require turbolinks
+//= require_tree .
