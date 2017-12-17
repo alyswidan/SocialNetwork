9a6fdfb3c0b1f3c4e0ede8c1515a8c579e779696
mahinourelsheikh <mahinour-elsheikh@hotmail.com>
2017-12-17 04:40:35 -0800

    post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,18 +4,22 @@ class PostTest < ActiveSupport::TestCase
     def setup
       @user = users(:michael)
 # This code is not idiomatically correct.
-      @Post = Post.new(caption: "Lorem ipsum", user_id: @user.id)
+      @post = Post.new(caption: "Lorem ipsum", user_id: @user.id)
     end
     test "should be valid" do
-      assert @Post.valid?
+      assert @post.valid?
     end
     test "user id should be present" do
-      @Post.user_id = nil
-      assert_not @Post.valid?
+      @post.user_id = nil
+      assert_not @post.valid?
     end
     test "caption should be at most 500 characters" do
-      @Post.content = "a" * 501
-      assert_not @Post.valid?
+      @post.caption = "a" * 501
+      assert_not @post.valid?
     end
+    test "order should be most recent first" do
+      assert_equal Post.first,posts(:most_recent)
+    end
+
   end
 
