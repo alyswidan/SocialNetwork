5b7c79e6dd03e49ee159de11b67c634099ef8922
mahinourelsheikh <mahinour-elsheikh@hotmail.com>
2017-12-17 04:06:01 -0800

    additional updates

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+    def setup
+      @user = users(:michael)
+# This code is not idiomatically correct.
+      @Post = Post.new(caption: "Lorem ipsum", user_id: @user.id)
+    end
+    test "should be valid" do
+      assert @Post.valid?
+    end
+    test "user id should be present" do
+      @Post.user_id = nil
+      assert_not @Post.valid?
+    end
+    test "caption should be at most 500 characters" do
+      @Post.content = "a" * 501
+      assert_not @Post.valid?
+    end
+  end
+
