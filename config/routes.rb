6bfbbe963ca902e1fd43d06a8f5e380e8c3e2dea
alyswidan <swidan12340@gmail.com>
2017-12-17 19:59:57 +0200

    merged

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
-
 Rails.application.routes.draw do
   get 'sessions/new'
 
   get 'users/new'
 
-<<<<<<< HEAD
   root 'static_pages#home'
 
-  get    'help'=> 'static_pages#help'
-  get    'home'=> 'static_pages#home'
-  get    'about'=> 'static_pages#about'
+  get    'help' => 'static_pages#help'
+  get    'home' => 'static_pages#home'
+  get    'about' => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
-=======
-  root 'users#new'
-
-  get    'help'=> 'users#new'
-  get    'home'=> 'users#new'
-  get    'about'=> 'users#new'
-  get    'contact' => 'users#new'
->>>>>>> user_profile
   get    'signup' => 'users#new'
   get    'login'  => 'sessions#new'
   post   'login'  => 'sessions#create'
