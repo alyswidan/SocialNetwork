cdc31619d15f8a66da3ae1aa48ac33f8ef0160c9
zeinanada <es-zeina.mahmoud14151@alexu.edu.eg>
2017-12-23 15:32:28 +0200

    AJAX send/unsend request

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,17 +1,15 @@
 class FriendsController < ApplicationController
 
   def create
-    @user = User.find(params[:other_user_id])
-    helpers.current_user.send_request(@user)
-    respond_to do |format|
-      format.html{ redirect_to @user}
-      format.js
-    end
+
   end
   def destroy
     helpers.current_user.remove_friend(params[:user_id])
     redirect_to User.find(params[:user_id])
-
+    respond_to do |format|
+      format.html{ redirect_to current_user}
+      format.js
+    end
 
   end
 end
