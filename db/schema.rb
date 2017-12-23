895a085ca6dc88f4a44c8f236716f9ef3bcca4d5
zeinanada <es-zeina.mahmoud14151@alexu.edu.eg>
2017-12-23 18:27:12 +0200

    friends added to template

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171217182345) do
-
+ActiveRecord::Schema.define(version: 20171219174914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +66,6 @@ ActiveRecord::Schema.define(version: 20171217182345) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "picture"
-
   end
 
   create_table "users", force: :cascade do |t|
