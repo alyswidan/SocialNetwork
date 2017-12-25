08e5c4ebda4002ed73683ce7396ce14564025bb0
alyswidan <swidan12340@gmail.com>
2017-12-25 20:55:57 +0200

    modified migration to work on heroku

--- db/migrate/20171217112321_add_index_to_posts.rb
+++ db/migrate/20171217112321_add_index_to_posts.rb
@@ -1,5 +1,12 @@
 class AddIndexToPosts < ActiveRecord::Migration[5.1]
   def change
+
+    add_column :posts,:user_id,:integer
+    add_foreign_key :posts, :users,
+                    column: :user_id,
+                    primary_key: :id,
+                    on_delete: :cascade
+
     add_index :posts, [:user_id, :created_at]
   end
 end
